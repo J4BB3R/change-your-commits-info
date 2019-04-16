5fb2b9ab0436e4c2ca62bb16173c87b9da416221
J4BB3R <johanmaurel@protonmail.com>
2019-04-16 17:15:10 +0200

    Ad script

---
+++ change.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="<Your_Old_Email>"
+OLD_NAME="Old_Name"
+CORRECT_NAME="New_Name"
+CORRECT_EMAIL="<Your_New_Email>"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
